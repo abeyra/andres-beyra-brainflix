b411b1a1690133c1fc139685e6e1068883fe5800
andres beyra <andresbeyra@gmail.com>
2022-02-22 21:58:01 -0500

    began work on header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import './Header.scss';
+import '../../assets/logo/BrainFlix-logo.svg';
+
+function Header(props) {
+
+    return (
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    )
+} 
+
+export default Header;
