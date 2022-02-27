596319cab519a46ac83bc13240c3f40c24183677
andres beyra <andresbeyra@gmail.com>
2022-02-26 22:52:09 -0500

    added handleVideoChange function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './reset.css';
 import './App.scss';
+import React from 'react';
+import Video_data from './data/video-details.json';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Video_details from './components/Video_details/Video_details';
@@ -7,17 +9,36 @@ import Comments_form from './components/Comments_form/Comments_form';
 import Video_comments from './components/Video_comments/Video_comments';
 import Video_list from './components/Video_list/Video_list';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <Video_details />
-      <Comments_form />
-      <Video_comments />
-      <Video_list />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    Videos: Video_data,
+    currentVideo: Video_data[0] 
+  }
+
+  // handleVideoChange = (id) => {
+  //   const newVideoId = this.state.Videos.findIndex(video => === video.id)
+  //   this.setState({
+  //     currentVideo: this.state.Videos[newVideoId]
+  //   })
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Hero 
+        
+        />
+        <Video_details 
+      
+        />
+        <Comments_form />
+        <Video_comments />
+        <Video_list />
+      </div>
+    );
+  }
 }
 
 export default App;
