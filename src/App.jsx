8f21741f5783955d1e98a3a1aa3b429ab0078bff
andres beyra <andresbeyra@gmail.com>
2022-03-06 16:31:45 -0500

    removed unnecessary import of react router prop into app component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import UploadPage from './components/UploadPage/UploadPage';
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
 
