8cd90b1ba2fd0d90a4a412b2f3b73148ffc9eead
andres beyra <andresbeyra@gmail.com>
2022-03-05 19:31:20 -0500

    deleted superfluous data folder and files inside. added cursor pointer css property to upload button in header component. styled uploadpage component for mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,41 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import UploadPage from './components/UploadPage/UploadPage';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 
 function App() {
 
-    return (
-      <>
-        <div className="App">
-          <BrowserRouter>
-            <Header />
-              <Switch>
-                <Route path="/" exact component={Main}/>
-                <Route path="/upload" component={UploadPage} />
-                <Route path="/videos/:id" component={Main}/>
-              </Switch>
-          </BrowserRouter>
-        </div>
-      </>
-    )
+  // let submit = (event) => {
+  //   alert("Video Uploaded!");
+  //   <Redirect to="/" />
+  // }
+
+  let handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target; 
+        const title = form.videoTitle.value;
+        const description = form.videoDescription.value;
+        if(title && description){
+            alert("Upload Successful!")
+        } else {
+            alert("fill out form!")
+        }
+    }
+
+  return (
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Header />
+            <Switch>
+              <Route path="/" exact component={Main}/>
+              <Route path="/upload" component={UploadPage} handleSubmit={handleSubmit}/>
+              <Route path="/videos/:id" component={Main}/>
+            </Switch>
+        </BrowserRouter>
+      </div>
+    </>
+  )
 }
 
 export default App;
--- src/components/UploadPage/UploadPage.jsx
+++ src/components/UploadPage/UploadPage.jsx
@@ -1,25 +1,30 @@
 import './UploadPage.scss';
 import Bike from '../../assets/images/Upload-video-preview.jpg';
 import PublishIcon from '../../assets/icons/publish.svg';
+import {Redirect} from 'react-router-dom';
 
-export default function UploadPage () {
+  
+
+export default function UploadPage ({handleSubmit}) {
+
+  
 
     return (
         <>
         <section className="Upload">
-            <h1>Upload Video</h1>
-            <form action="" className="Upload__form">
+            <h1 className="Upload__title">Upload Video</h1>
+            <form action="" className="Upload__form" onSubmit={handleSubmit}>
                 <label className="Upload__form-label" htmlFor="">VIDEO THUMBNAIL</label>
                 <img className="Upload__form-image" src={Bike} alt="upload bike" />
                 <label className="Upload__form-label" htmlFor="upload-input">TITLE YOUR VIDEO</label>
-                <input className="Upload__form-input" type="text" placeholder="Add a title to your video" id="upload-input"/>
+                <input className="Upload__form-input" type="text" placeholder="Add a title to your video" id="upload-input" name="videoTitle"/>
                 <label className="Upload__form-label" htmlFor="upload-textarea">ADD A VIDEO DESCRIPTION</label>
-                <textarea className="Upload__form-textarea" name="" id="upload-textarea" placeholder="Add a description to your video"></textarea>
+                <textarea className="Upload__form-textarea" name="" id="upload-textarea" placeholder="Add a description to your video" name="videoDescription"></textarea>
                 <div className="Upload__form-publish-container">
                     <button className="Upload__form-publish-container-button">PUBLISH</button>
                     <img className="Upload__form-publish-container-icon" src={PublishIcon} alt="publish icon" />
                 </div>
-                <p>CANCEL</p>
+                <button className="Upload__cancel-button">CANCEL</button>
             </form>
         </section>
         </>
