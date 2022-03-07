754209e691619b6daf7028b3f5766c74cedb0ce4
andres beyra <andresbeyra@gmail.com>
2022-03-06 20:01:55 -0500

    removed unnecessary commented out line of code from video details component. finished styling for tablet and desktop view for upload page.

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -34,17 +34,24 @@ export default class UploadPage extends Component {
                 <section className="Upload">
                     <h1 className="Upload__title">Upload Video</h1>
                     <form action="" className="Upload__form" onSubmit={this.handleSubmit}>
-                        <label className="Upload__form-label" htmlFor="">VIDEO THUMBNAIL</label>
-                        <img className="Upload__form-image" src={Bike} alt="upload bike" />
-                        <label className="Upload__form-label" htmlFor="upload-input">TITLE YOUR VIDEO</label>
-                        <input className="Upload__form-input" type="text" placeholder="Add a title to your video" id="upload-input" name="videoTitle"/>
-                        <label className="Upload__form-label" htmlFor="upload-textarea">ADD A VIDEO DESCRIPTION</label>
-                        <textarea className="Upload__form-textarea" name="" id="upload-textarea" placeholder="Add a description to your video" name="videoDescription"></textarea>
+                        <div className="Upload__form-content-container">
+                            <div className="Upload__form-image-container">
+                                <label className="Upload__form-label" htmlFor="">VIDEO THUMBNAIL</label>
+                                <img className="Upload__form-image" src={Bike} alt="upload bike" />
+                            </div>
+                            <div className="Upload__form-inputs-container">
+                                <label className="Upload__form-label" htmlFor="upload-input">TITLE YOUR VIDEO</label>
+                                <input className="Upload__form-input" type="text" placeholder="Add a title to your video" id="upload-input" name="videoTitle"/>
+                                <label className="Upload__form-label" htmlFor="upload-textarea">ADD A VIDEO DESCRIPTION</label>
+                                <textarea className="Upload__form-textarea" name="" id="upload-textarea" placeholder="Add a description to your video" name="videoDescription"></textarea>
+                            </div>
+                        </div>
                         <div className="Upload__form-publish-container">
                             <button type='submit' className="Upload__form-publish-container-button">PUBLISH</button>
                             <img className="Upload__form-publish-container-icon" src={PublishIcon} alt="publish icon" />
+                            <button className="Upload__form-publish-container-cancel-button">CANCEL</button>
                         </div>
-                        <button className="Upload__cancel-button">CANCEL</button>
+                        
                     </form>
                 </section>
             </>
